62bbaab0bc4d7f1be93d1482ce4c80e16230a7b0
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-01-08 23:27:45 +0100

    Add post method to /translate/ route and parso body to json.

--- translate/views.py
+++ translate/views.py
@@ -1,8 +1,18 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def index(request, *args, **kwargs):
-    return HttpResponse('Hello, world')
+    if request.method == 'GET':
+        return HttpResponse('Welcome to translate ')
+    elif request.method == 'POST':
+        # print(f"{request.body}")
+        json_data = json.loads(request.body)
+        return HttpResponse(f"{json_data}")
