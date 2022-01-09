5e1c70139a8360d4d183197f60a4baef691f20f3
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-01-09 17:55:11 +0100

    Install pytest and configure project to use both test frameworks.

--- translate/tests.py
+++ translate/tests.py
@@ -1,3 +1,4 @@
+import pytest
 from django.test import TestCase
 from .utils.rle_translator import expand_row
 
@@ -11,8 +12,23 @@ class UtilTest(TestCase):
 
         self.assertEqual(result, ['o', 'o'])
 
-    def test_expand_row_int_char_notation(self):
+    # case 1
+    row_1 = ['2bo2b2o']
+    expected = ['2b', 'o', '2b', '2o']
+    case_row_1 = row_1, expected
+
+    # case 2
+    row_2 = ['112bo2b2o']
+    expected_2 = ['112b', 'o', '2b', '2o']
+    case_row_2 = row_2, expected_2
+
+    @pytest.fixture(autouse=True)
+    def prepare_expand_row(self):
+        self.expand_row = expand_row
+
+    def test_expand_row_int_char_notation_1(self):
         row = ['2bo2b2o']
+        expected = ['2b', 'o', '2b', '2o']
         result = expand_row(row)
 
-        self.assertEqual(result, ['2b', 'o', '2b', '2o'])
+        self.assertEqual(result, expected)
